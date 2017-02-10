45f19df40be82a1132ba5bb10017d24364bd3a55
jayden <jxdjayden@gmail.com>
2017-02-10 14:53:52 +0800

    update observer

--- javascript-design-pattern/observer.js
+++ javascript-design-pattern/observer.js
@@ -49,11 +49,29 @@ class Event {
     on(type, fn) {
         this.addEventListener(type, fn);
     }
+
+    once(type, fn) {
+        let a = true;
+        function tmp(...args) {
+            if (a) {
+                a = false;
+                fn.apply(null, args);
+            }
+        }
+        this.addEventListener(type, tmp);
+    }
 }
 
 const newEvent = new Event();
 
 newEvent.addEventListener('test', (...args) => { console.log(args) });
 newEvent.on('test', (...args) => { console.log(args) });
+newEvent.once('test', (...args) => { console.log(args, 'once') });
+
 newEvent.emit('test', 'test1', 'test2');
+newEvent.emit('test', 'test1', 'test2');
+
 newEvent.remove('test');
+
+newEvent.emit('test', 'test1', 'test2');
+
