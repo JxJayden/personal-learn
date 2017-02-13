f1c04e36fa57133e65056386b69048e9f1fe5b5b
jayden <jxdjayden@gmail.com>
2017-02-13 23:33:48 +0800

    update quicksort

--- algorithms-learn/sorting/quick.js
+++ algorithms-learn/sorting/quick.js
@@ -2,7 +2,27 @@
 
 var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 
-function quickSort(arr, left, right) {
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    var i = 0,
+        pivotIndex = Math.floor(arr.length / 2),
+        pivot = arr.splice(pivotIndex, 1)[0],
+        left = [],
+        right = [];　
+
+    for (i; i < arr.length; i++) {　　　　
+        if (arr[i] < pivot) {　　　　　　
+            left.push(arr[i]);　　　　
+        } else {　　　　　　
+            right.push(arr[i]);　　　　
+        }　　
+    }
+    return quickSort(left).concat([pivot], quickSort(right));
+}
+
+function quickSort1(arr, left, right) {
     var len = arr.length,
         partitionIndex,
         left = typeof left === 'number' ? left : 0,
